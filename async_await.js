2d3a48b6ec395ea3924727e6f469c4a0d4c33c0d
izaack107 <izaack107@gmail.com>
2023-08-31 14:31:55 -0600

    Async y Await

---
+++ async_await.js
@@ -0,0 +1,41 @@
+async function hola(nombre) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(() => {
+            console.log("Hola, "+ nombre)
+            resolve(nombre);
+        }, 1000);
+    });
+}
+
+async function adios(nombre) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("Adios ", nombre)
+            resolve();
+        }, 1000);
+    })
+
+}
+
+async function hablar(nombre) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("BLa Bla Bla...")
+            resolve(nombre);
+            reject("Hay un error");
+        }, 1000);
+    })
+}
+
+async function main(params) {
+    let nombre =  await hola("Isaac");
+
+    await hablar()
+    await hablar()
+    await hablar()
+    await adios(nombre)
+}
+
+console.log("Empezamos proceso")
+main();
+console.log("Terminamos proceso")
