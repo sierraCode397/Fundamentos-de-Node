53a0f601a3787a32a124a80d32dc2b5df7769602
izaack107 <izaack107@gmail.com>
2023-08-31 11:51:05 -0600

    Callback

---
+++ callback.js
@@ -0,0 +1,21 @@
+function hola(nombre, miCallback) {
+    setTimeout(() => {
+        console.log("Hola, "+ nombre)
+        miCallback(nombre);
+    }, 1000);
+}
+
+function adios(nombre, otroCallback) {
+    setTimeout(() => {
+        console.log("Adios ", nombre)
+        otroCallback();
+    }, 1000);
+}
+
+console.log("Iniciando proceso...");
+hola("Isaac", function (nombre) {
+    console.log("Primer proceso terminado...")
+    adios(nombre, function () {
+        console.log("Terminando proceso...");
+    } );
+});
