3fe7c3904981459c4b48671b9d50cdbbaf1ee46f
izaack107 <izaack107@gmail.com>
2023-09-04 21:39:57 -0600

    Mas Informacion a la clase Stream

--- memoria/stream.js
+++ memoria/stream.js
@@ -2,29 +2,38 @@ const fs = require("fs");
 const stream = require('stream');
 const util = require('util')
 
-let data = '';
-let readableStream = fs.createReadStream(__dirname + '/imput.txt');
 
-/* 
+//Stream de lectura
+let data = '';
+let readableStream = fs.createReadStream(__dirname + '/imput.txt'); //Entrada, tipo lectura. Van a venir datos
+
+
 readableStream.setEncoding('UTF8');
 readableStream.on('data', (chuck) => {
-    data += chuck;
-    //console.log(chuck);
+    data += chuck; //Va agregando la informacion que va llegando
+    //console.log(chuck.toString());
 });
 
 readableStream.on('end', () => {
     console.log(` Data: ${data}`);
 });
 
+//----------------------------------
+
+//Stream de escritura
+
 process.stdout.write('Hola ');
 process.stdout.write('que ');
 process.stdout.write('tal ');
- */
 
-const Transform = stream.Transform;
+//----------------------------------
+
+//Setream de transformacion o doble sentido
+
+const Transform = stream.Transform; 
 
 function Mayus() {
-    Transform.call(this);
+    Transform.call(this);//Se inicializa el llamado a la transformacion
 }
 
 util.inherits(Mayus, Transform);
